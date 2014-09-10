4df7d7e8274d6d836850e7f06ae17d3bc9719691
Gonzalo89 <gonzalo89@ymail.com>
2014-09-10 12:38:39 -0300

    [AUS00][RQ1]  Proyecto android creado
    Creo la clase Propiedad que es la que se va a mostrar en la ListView

---
+++ src/com/g7/mileemandroid/Propiedad.java
@@ -0,0 +1,35 @@
+package com.g7.mileemandroid;
+
+public class Propiedad {
+
+	private String direccion;
+	private String Descripcion;
+	private int antiguedad;
+	private String tipoOperacion;
+	private int precio;
+	private int superficie;
+	private int ambientes;
+	private int dormitorios;
+	private int expensas;
+	private String barrio;
+	private String tipoPropiedad;
+	
+	public Propiedad(String direccion, String descripcion, int antiguedad,
+			String tipoOperacion, int precio, int superficie, int ambientes,
+			int dormitorios, int expensas, String barrio, String tipoPropiedad) {
+		super();
+		this.direccion = direccion;
+		Descripcion = descripcion;
+		this.antiguedad = antiguedad;
+		this.tipoOperacion = tipoOperacion;
+		this.precio = precio;
+		this.superficie = superficie;
+		this.ambientes = ambientes;
+		this.dormitorios = dormitorios;
+		this.expensas = expensas;
+		this.barrio = barrio;
+		this.tipoPropiedad = tipoPropiedad;
+	}
+	
+		
+}
