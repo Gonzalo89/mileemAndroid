6f809f4c21ca65ddd38cb15bf6691ded75b1a821
rmato <rodrigo.nmato@gmail.com>
2014-09-26 00:24:01 -0300

    Avances con el mapa

--- mileemAndroid/src/com/g7/mileemandroid/Activites/MapaActivity.java
+++ mileemAndroid/src/com/g7/mileemandroid/Activites/MapaActivity.java
@@ -4,6 +4,13 @@ import com.g7.mileemandroid.R;
 import com.g7.mileemandroid.R.id;
 import com.g7.mileemandroid.R.layout;
 import com.g7.mileemandroid.R.menu;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -12,10 +19,31 @@ import android.view.MenuItem;
 
 public class MapaActivity extends ActionBarActivity {
 
+	static final LatLng staticLocation = new LatLng(53.551, 9.993);
+	private GoogleMap map;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mapa);
+		
+		// Initialize Map
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		    
+	if (map!=null){
+		Marker testMarker = map.addMarker(new MarkerOptions()
+		          .position(staticLocation)
+		          .title("Posiscion Hardcodeada")
+		          .snippet("Kiel is cool - descripcion")
+		          .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
+	}
+	
+    // Move the camera instantly to staticLocation with a zoom of 15.
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(staticLocation, 15));
+
+    // Zoom in, animating the camera.
+    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+		    
 	}
 
 	@Override
