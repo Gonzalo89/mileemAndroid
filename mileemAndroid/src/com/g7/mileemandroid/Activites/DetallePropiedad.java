0890781541a0c75cd3bd50df6debce5b4f094160
Gonzalo89 <gonzalo89@ymail.com>
2014-10-16 15:13:38 -0300

    Elimino modo landscape

--- mileemAndroid/src/com/g7/mileemandroid/Activites/DetallePropiedad.java
+++ mileemAndroid/src/com/g7/mileemandroid/Activites/DetallePropiedad.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.MeasureSpec;
 import android.view.ViewGroup;
--- mileemAndroid/src/com/g7/mileemandroid/Activites/MapaActivity.java
+++ mileemAndroid/src/com/g7/mileemandroid/Activites/MapaActivity.java
@@ -1,22 +1,16 @@
 package com.g7.mileemandroid.Activites;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.g7.mileemandroid.R;
-import com.g7.mileemandroid.R.id;
-import com.g7.mileemandroid.R.layout;
-import com.g7.mileemandroid.R.menu;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MapaActivity extends ActionBarActivity {
 
