471339e7568aa93a4c400b513a54fdf8994648a2
Gonzalo89 <gonzalo89@ymail.com>
2014-10-30 09:46:14 -0300

    Cargo video desde youtube, pero no anda

--- mileemAndroid/src/com/g7/mileemandroid/Activites/VideoFragment.java
+++ mileemAndroid/src/com/g7/mileemandroid/Activites/VideoFragment.java
@@ -1,10 +1,12 @@
 package com.g7.mileemandroid.Activites;
 
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.VideoView;
 
 import com.g7.mileemandroid.R;
 
@@ -18,4 +20,14 @@ public class VideoFragment extends Fragment {
 	         
 	        return rootView;
 	    }
+	   
+	   @Override
+	    public void onActivityCreated(Bundle bundle){		
+		super.onActivityCreated(bundle); 
+		VideoView videoView = (VideoView)getView().findViewById(R.id.videoView1);
+        //videoView.setVideoPath("/sdcard/blonde_secretary.3gp");
+		//videoView.setVideoURI(Uri.parse("www.youtube.com/embed/16F4Jz3dtJQ"));
+		videoView.setVideoPath("www.youtube.com/embed/16F4Jz3dtJQ");
+        videoView.start();  
+	   }
 }
