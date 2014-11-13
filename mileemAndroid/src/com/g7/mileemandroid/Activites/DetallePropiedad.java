506e62e70c6f7caff9aa6f430e697eb23d0982c5
rmato <rodrigo.nmato@gmail.com>
2014-11-12 22:03:46 -0300

    Terminado el Share

--- mileemAndroid/src/com/g7/mileemandroid/Activites/DetallePropiedad.java
+++ mileemAndroid/src/com/g7/mileemandroid/Activites/DetallePropiedad.java
@@ -1,14 +1,22 @@
 package com.g7.mileemandroid.Activites;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import android.app.ActionBar.LayoutParams;
 import android.app.Fragment;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.LabeledIntent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -27,6 +35,7 @@ import com.g7.mileemandroid.Model.AdapterDetallePropiedad;
 import com.g7.mileemandroid.Model.AtributoPropiedad;
 import com.g7.mileemandroid.Model.Propiedad;
 import com.g7.mileemandroid.Model.PropiedadSingleton;
+import com.google.android.gms.drive.internal.ap;
 
 public class DetallePropiedad extends Fragment {
 	
@@ -180,6 +189,99 @@ public class DetallePropiedad extends Fragment {
 //		Intent intent = new Intent( Intent.ACTION_CALL );
 //		intent.setData( Uri.parse(urlPhone) );
 //		this.startActivity(intent);
+	}
+	
+	public void onClickShare (View view) {
+
+		// Contenido
+		String content = "Mileem - Que te parece esta propiedad? Dirección: " + propiedad.getDireccion()+" "+propiedad.getNumero() + " Ambientes: " 
+		+ propiedad.getAmbientes() + " Precio: " + propiedad.getPrecio() + " Superficie: " + propiedad.getSuperficie();
+		String subject = "Propiedad en Mileem";
+		
+		// Control de apps instaladas
+		String appsNoInstaladas = "";
+		int count = 0;
+		try{
+		    ApplicationInfo info = getActivity().getPackageManager().getApplicationInfo("com.facebook.katana", 0 );
+		} catch( PackageManager.NameNotFoundException e ){
+		    appsNoInstaladas += "Facebook";
+		    count++;
+		}
+		try{
+		    ApplicationInfo info = getActivity().getPackageManager().getApplicationInfo("com.twitter.android", 0 );
+		} catch( PackageManager.NameNotFoundException e ){
+			if (count>0)
+				appsNoInstaladas += ", ";
+		    appsNoInstaladas += "Twitter";
+		    count++;
+		}
+		try{
+		    ApplicationInfo info = getActivity().getPackageManager().getApplicationInfo("com.google.android.apps.plus", 0 );
+		} catch( PackageManager.NameNotFoundException e ){
+			if (count>0)
+				appsNoInstaladas += ", ";
+		    appsNoInstaladas += "Google+";
+		    count++;
+		}
+		
+		if (count > 0) {
+			
+			String textoToast = (count>1?"Las aplicaciones " : "La aplicacion ") + appsNoInstaladas + ", "+ (count>1? "no se encuentran disponibles" : "no se encuentra disponible") +" para compartir.";
+			Toast.makeText(getActivity(), textoToast, Toast.LENGTH_SHORT).show();
+		}
+		
+	    Resources resources = getResources();
+
+	    Intent emailIntent = new Intent();
+	    emailIntent.setAction(Intent.ACTION_SEND);
+	    // Native email client doesn't currently support HTML, but it doesn't hurt to try in case they fix it
+	    emailIntent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(content));
+	    emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+	    emailIntent.setType("message/rfc822");
+
+	    PackageManager pm = getActivity().getPackageManager();
+	    Intent sendIntent = new Intent(Intent.ACTION_SEND);     
+	    sendIntent.setType("text/plain");
+
+
+	    Intent openInChooser = Intent.createChooser(emailIntent, "Compartir");
+
+	    List<ResolveInfo> resInfo = pm.queryIntentActivities(sendIntent, 0);
+	    List<LabeledIntent> intentList = new ArrayList<LabeledIntent>();        
+	    for (int i = 0; i < resInfo.size(); i++) {
+	        // Extract the label, append it, and repackage it in a LabeledIntent
+	        ResolveInfo ri = resInfo.get(i);
+	        String packageName = ri.activityInfo.packageName;
+	        if(packageName.contains("android.email")) {
+	            emailIntent.setPackage(packageName);
+	        } else if(packageName.contains("twitter") || packageName.contains("facebook") || packageName.contains("com.google.android.apps.plus") || packageName.contains("android.gm")) {
+	            Intent intent = new Intent();
+	            intent.setComponent(new ComponentName(packageName, ri.activityInfo.name));
+	            intent.setAction(Intent.ACTION_SEND);
+	            intent.setType("text/plain");
+	            if(packageName.contains("twitter")) {
+	                intent.putExtra(Intent.EXTRA_TEXT, content);
+	            } else if(packageName.contains("facebook")) {
+	                intent.putExtra(Intent.EXTRA_TEXT, content);
+	            } else if(packageName.contains("com.google.android.apps.plus")) {
+	                intent.putExtra(Intent.EXTRA_TEXT, content);
+	            } else if(packageName.contains("android.gm")) {
+	                intent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(content));
+	                intent.putExtra(Intent.EXTRA_SUBJECT, subject);               
+	                intent.setType("message/rfc822");
+	            }
+
+	            intentList.add(new LabeledIntent(intent, packageName, ri.loadLabel(pm), ri.icon));
+	        }
+	    }
+
+	    // convert intentList to array
+	    LabeledIntent[] extraIntents = intentList.toArray( new LabeledIntent[ intentList.size() ]);
+
+	    openInChooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, extraIntents);
+	    startActivity(openInChooser);       
+
+		
 	}
 	
 	
